4249b40933eb78d93b808475df2e82b2542d420a
CumTZN <artem.nazarov.0208@mail.ru>
2022-06-08 01:15:27 +0300

    Weapon/WLvl

---
+++ Rpg_fr/Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collidable
+{
+    public int damagePoint = 1;
+    public float pushForce = 2.0f;
+
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    private float cooldown = 0.5f;
+    private float lastSwing;
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - lastSwing > cooldown)
+            {
+                lastSwing = Time.time;
+                Swig();
+            }
+        }
+
+    }
+    private void Swig()
+    {
+        Debug.Log("Swig");
+    }
+
+}
