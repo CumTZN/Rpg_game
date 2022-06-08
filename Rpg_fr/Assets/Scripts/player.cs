ebcb41d7bd2c34087afaeb60ee1855fe32ea18f3
CumTZN <artem.nazarov.0208@mail.ru>
2022-06-08 08:32:40 +0300

    Enemy_takeDamage

--- Rpg_fr/Assets/Scripts/player.cs
+++ Rpg_fr/Assets/Scripts/player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //[RequireComponent(typeof(BoxCollider2D))]
-public class Player : MonoBehaviour
+public class Player : Fighter 
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
